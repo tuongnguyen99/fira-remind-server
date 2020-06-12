93223ff8141d817fb13de8946f9a833977f005c7
tuongnguyen99 <57837337+tuongnguyen99@users.noreply.github.com>
2020-06-12 08:18:33 +0700

    Update uploadRoutes.js

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const uploadFiles = require('../controllers/uploadController');
-console.log(uploadFiles);
 
 router.post('/', (req, res) => {
   uploadFiles(req, res);
