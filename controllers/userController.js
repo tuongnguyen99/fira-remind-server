e804dec9537ae4950c35091a66c24643712a60b9
tuongnguyen <nctuong99@gmail.com>
2020-06-17 18:26:17 +0700

    update login - add type

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,7 @@ const cvtToResponse = (user) => {
   return (response = {
     id: user.id,
     username: user.username,
+    type: user.type,
     passwordChanged: user.password_status,
     hasAccessToken: user.access_token ? 1 : 0,
     hasRefreshToken: user.refresh_token ? 1 : 0,
@@ -13,18 +14,14 @@ const cvtToResponse = (user) => {
 };
 
 const login = function login(req, res) {
-  const { username, password } = req.body;
+  const { username, password, type } = req.body;
   const query = `SELECT * FROM USER WHERE USERNAME = '${username}' LIMIT 1`;
   cn.query(query, (err, results) => {
-    console.log(results);
-
     if (err) return res.status(400).send(err.message);
 
-    if (results.length === 0 || !compare(password, results[0].password))
-      return res.status(404).send({ message: 'user not found' });
-
     const user = results[0];
-
+    if (!user || !compare(password, user.password) || user.type !== type)
+      return res.status(404).send({ message: 'user not found' });
     res.send(cvtToResponse(user));
   });
 };
