944326316eaadc80ddbe5fc6e2c871db70ceebdb
tuongnguyen99 <57837337+tuongnguyen99@users.noreply.github.com>
2020-06-17 15:31:47 +0700

    Create time.js

---
+++ utils/time.js
@@ -0,0 +1,13 @@
+
+  function cvtToDateStr(date, dft) {
+    if (!date || typeof date !== 'number') {
+      return dft || '';
+    }
+    const utcTime = Math.floor(date - 25569) * 86400;
+    const dateInfo = new Date(utcTime * 1000);
+    return `${dateInfo.getFullYear()}-${
+      dateInfo.getMonth() + 1
+    }-${dateInfo.getDate()}`;
+  }
+  
+  module.exports = cvtToDateStr;
