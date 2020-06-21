927fd3974d03510e73fca8bc8026a80387326c4c
thangtranit1999 <ngocthangkt27@gmail.com>
2020-06-21 18:29:10 +0700

    inspect get-post

--- controllers/inspectController.js
+++ controllers/inspectController.js
@@ -11,6 +11,25 @@ async function list(req, res){
     })
 }
 
+function evaluate(req, res){
+    const data = req.body;
+    const query = `INSERT INTO thanhtra (id, m_ttra, giovipham, sisothucte, gv_botiet, gv_ditre, gv_nghisom, gv_saiten, gv_daykhongthongbao, chitiet, id_tkb) VALUES (NULL, '${data.ms_thanhtra}', '${data.giovipham}', '${data.sisothucte}', '${data.gv_botiet}', '${data.gv_ditre}', '${data.gv_nghisom}', '${data.gv_saiten}', '${data.gv_daykhongbao}', '${data.chitiet}', '${data.id_tkb}')`;
+    cn.query(query, err=>{
+        if(err) return res.status(400).send(err.message);
+        res.send({
+            message:"Cập nhật thành công"
+        })
+    })
+}
+function getEvaluate(req, res){
+    const id_tkb = req.params.id_tkb;
+    const query = `SELECT * FROM thanhtra WHERE id_tkb='${id_tkb}'`;
+    cn.query(query,(err, results)=>{
+        if(err) return res.status(400).send(err.message);
+        res.send(results);
+    })
+}
+
 module.exports = {
-    list
+    list, evaluate, getEvaluate
 }
--- routes/inspectRoute.js
+++ routes/inspectRoute.js
@@ -5,5 +5,10 @@ const inspect = require('../controllers/inspectController');
 route.get('/list/:day', (req, res)=>{
     inspect.list(req, res);
 })
-
+route.get('/evaluate/:id_tkb', (req, res)=>{
+    inspect.getEvaluate(req, res);
+})
+route.post('/evaluate', (req, res)=>{
+    inspect.evaluate(req, res);
+})
 module.exports = route;
