6a4c8bd41f5459164b0dc5217dcd7746b698808e
thangtranit1999 <ngocthangkt27@gmail.com>
2020-06-27 23:23:38 +0700

    update list roomuse

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,17 +1,15 @@
 const { createConnection } = require('../db/db');
 const cn = createConnection();
+const moment = require('moment');
+const { query, text } = require('express');
 
 async function roomUse(req, res) {
-    var day = req.params;
-    day = day.day.replace(/-/g, "/");
-    const p_sdung = await dataUseRoom(day, res);
-    res.status(200).send(p_sdung);
+    var day = req.params.day;
+    res.send(await dataUseRoomGd(day, res))
 }
 async function emptyRoom(req, res) {
-    var day = req.params;
-    day = day.day.replace(/-/g, "/");
-    const phong = await dataEmptyRoom(day, res);
-    res.status(200).send(phong);
+    var day = req.params.day;
+    res.send(await dataEmptyRoomGD(day, res));
 }
 async function statusRoom(req, res) {
     var day = req.params;
@@ -19,8 +17,9 @@ async function statusRoom(req, res) {
     const data = await dataStatusRoom(day, res);
     res.status(200).send(data);
 }
+// dữ liệu hiển thị
 
-function dataUseRoom(day, res) {
+function dataUseRoomGd(day, res) {
     const data = new Promise(tv => {
         const query = "select * from tkb";
         cn.query(query, (err, results) => {
@@ -58,7 +57,7 @@ function dataUseRoom(day, res) {
     })
     return data;
 }
-function dataEmptyRoom(day, res) {
+function dataEmptyRoomGD(day, res) {
     const data = new Promise(tv => {
         const query = "select * from phong";
         cn.query(query, async (err, results) => {
@@ -67,7 +66,7 @@ function dataEmptyRoom(day, res) {
             const tkb = await dataUseRoom(day, res);
             const stringJson = JSON.stringify(tkb);
             phong.forEach((element, i) => {
-                if(stringJson.indexOf(element.t_phong)!== -1){
+                if (stringJson.indexOf(element.t_phong) !== -1) {
                     phong.splice(i, 1);
                 }
             });
@@ -76,12 +75,23 @@ function dataEmptyRoom(day, res) {
     });
     return data;
 }
+
 room = (opject, st) => {
     return {
         id: opject.id,
         t_phong: opject.t_phong,
         khu: opject.khu,
         status: st
+    }
+}
+
+cvtRoom = (phong, s, c, t,id) => {
+    return {
+        id:id,
+        t_phong: phong,
+        b_sang: s,
+        b_chieu: c,
+        b_toi: t
     }
 }
 async function dataStatusRoom(day, req, res) {
@@ -97,17 +107,126 @@ async function dataStatusRoom(day, req, res) {
     const stringJson = JSON.stringify(empty);
     data.forEach((element, i) => {
         var st;
-        if(stringJson.indexOf(element.t_phong)!== -1){
+        if (stringJson.indexOf(element.t_phong) !== -1) {
             st = "EMPTY";
-            statusInRoom.push(room(data[i],st));
-        }else{
+            statusInRoom.push(room(data[i], st));
+        } else {
             st = "USING";
-            statusInRoom.push(room(data[i],st));
+            statusInRoom.push(room(data[i], st));
         }
     });
     return statusInRoom;
 }
+
+//Dữ liệu dụng xếp phòng
+
+async function listEmptyRoom(req, res){
+    res.send(await phongcothedung(req, res))
+}
+
+async function arrangeRoom(req, res){
+    const{t_phong, ngay, n_dung, b_sang,b_chieu,b_toi} = req.body;
+    console.log(n_dung);
+}
+
+async function phongcothedung(req, res) {
+    const day = req.params.day;
+    const phongtrong = await dataEmptyRoom(day, res);
+    const phongsudung = await dataUseRoom(day, res);
+    const phongcothedung = new Array();
+    phongsudung.forEach(element => {
+        if (element.b_sang === true && element.b_chieu === true) {
+
+        }else{
+            phongcothedung.push(element);
+        }
+    })
+    phongtrong.forEach(element=>{
+        phongcothedung.push(cvtRoom(element, false, false, false));
+    })
+    const kq = new Array();
+    phongcothedung.forEach((element,i)=>{
+        kq.push(cvtRoom(element.t_phong,element.b_sang, element.b_chieu, element.b_toi,i+1));
+    })
+    return kq;
+}
+
+async function dataUseRoom(day, res) {
+    const sql = `SELECT * FROM phong, tkb_gvien WHERE tkb_gvien.phong = phong.t_phong AND tkb_gvien.ngay = '${day}'`;
+    const dataRoom = await new Promise(tv => {
+        cn.query(sql, (err, results) => {
+            if (err) return res.status(400).send(err.message);
+            tv(results)
+        })
+    })
+    var dataXepPhong = await new Promise(tv => {
+        const query = `SELECT * FROM p_sdung WHERE ngay='${day}'`;
+        cn.query(query, (err, results) => {
+            if (err) return res.status(400).send(err.message);
+            tv(results);
+        })
+    })
+    var useRoom = new Array();
+    dataRoom.forEach(element => {
+        useRoom.push(element.phong)
+    })
+    useRoom = useRoom.filter((item, index) => {
+        return useRoom.indexOf(item) === index;
+    })
+    const kq = new Array()
+    useRoom.forEach((element, i) => {
+        const m = new Array()
+        dataRoom.forEach(el => {
+            if (el.phong === element) {
+                m.push(el.t_bdau);
+            }
+        })
+        if (m.length === 2) {
+            kq.push(cvtRoom(element, true, true, false));
+        } else {
+            if (m[0] === 1) {
+                kq.push(cvtRoom(element, true, false, false))
+            }
+            if (m[0] === 7) {
+                kq.push(cvtRoom(element, false, true, false))
+            }
+            if (m[0] === 13) {
+                kq.push(cvtRoom(element, false, false, true))
+            }
+        }
+    })
+    const mapBoolean = { 1: true, 0: false };
+    dataXepPhong.forEach(element => {
+        kq.push(cvtRoom(element.t_phong, mapBoolean[element.b_sang], mapBoolean[element.b_chieu], mapBoolean[element.b_toi]));
+    });
+    return kq;
+}
+async function dataEmptyRoom(day, res) {
+    const p_sdung = await dataUseRoom(day, res);
+    const t_phong = new Array();
+    p_sdung.forEach(element => {
+        t_phong.push(element.t_phong)
+    });
+    const dataPhong = await new Promise(tv => {
+        const sql = `SELECT * FROM phong`;
+        cn.query(sql, (err, results) => {
+            if (err) return res.status(400).send(err.message);
+            tv(results);
+        })
+    })
+    const room = new Array();
+    dataPhong.forEach(element => {
+        room.push(element.t_phong);
+    })
+    // console.log(room);
+    t_phong.forEach(element => {
+        const vt = room.indexOf(element);
+        room.splice(vt, 1);
+    })
+    return room;
+}
+
 //dataStatusRoom();
 module.exports = {
-    roomUse, emptyRoom, statusRoom
+    roomUse, emptyRoom, statusRoom, listEmptyRoom, arrangeRoom
 }
--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const roomStatus = require('../controllers/roomController');
 
+//hiển thị
 router.get('/roomuse/:day', (req, res)=>{
     roomStatus.roomUse(req, res);
 })
@@ -12,4 +13,11 @@ router.get('/statusroom/:day', (req, res)=>{
     roomStatus.statusRoom(req, res);
 })
 
+//xử lý
+router.get('/listemptyroom/:day', (req, res)=>{
+    roomStatus.listEmptyRoom(req, res);
+})
+router.post('/arrangeroom', (req, res)=>{
+    roomStatus.arrangeRoom(req, res);
+})
 module.exports = router;
